3e3240601825d1513d3a68eeb69782fda317f8fb
smessie <smessie@smessie.com>
2023-09-01 09:17:51 +0200

    chore: Remove boilerplate code and create beginning of ScholarlyBrowser component

---
+++ src/components/ScholarlyBrowser.vue
@@ -0,0 +1,22 @@
+<template>
+  <MDBContainer>
+    <h1>Scholarly Browser</h1>
+  </MDBContainer>
+</template>
+
+<script lang="ts">
+import {MDBContainer} from "mdb-vue-ui-kit";
+
+export default {
+  name: "ScholarlyBrowser",
+  components: {
+    MDBContainer,
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  margin-top: 2rem;
+}
+</style>
